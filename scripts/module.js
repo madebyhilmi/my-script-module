bed57690efde7c5deddbef6a916d09acad43ba43
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2022-01-30 11:27:06 -0700

    Add first commit

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,14 @@
-Hooks.once('init', async function() {
-
+Hooks.once('init', async function () {
+    console.log("Initializing...");
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.once('ready', async function () {
+    console.log("Ready!");
 });
+
+Hooks.on("renderChatMessage", (message, html, data) => {
+    if (message.roll.result != null) {
+        const result = message.roll.result
+        console.log(result)
+    }
+})
